2d56fc4170acb61cc307af26a393960404a7119b
Sloop <66267333+spallis93@users.noreply.github.com>
2023-01-30 20:38:42 -0500

    navbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import './app.css';
+import Navbar from "./Navbar";
 import Project1 from "./Project1";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project1" element={<Project1 />} />
